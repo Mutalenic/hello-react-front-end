63f4ff48ff2488d8ccee01cdff9c6ef205414067
Nicholas Mutale <nicomutale@gmail.com>
2022-07-25 14:45:38 +0200

    Update configureStore

--- src/Redux/configureStore.js
+++ src/Redux/configureStore.js
@@ -1,6 +1,7 @@
 import {legdacy_createStore as createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from "@reduxjs/toolkit/dist/devtoolsExtension";
+import { ADD_GREETING } from '../components/HelloWorld';
 
 const initialState = {
     greetings: [
@@ -13,7 +14,7 @@ const initialState = {
 
 function reducer(state = initialState, action) {
     switch (action.type) {
-        case 'ADD_GREETING':
+        case ADD_GREETING:
             return {
                 ...state,
                 greetings: [...state.greetings, action.json],
