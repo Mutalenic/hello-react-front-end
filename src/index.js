3ab8bb57778325729d57d976c5e8edd82199f278
Nicholas Mutale <nicomutale@gmail.com>
2022-07-27 01:27:57 +0200

    Update extension

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { Provider } from 'react-redux';
 import App from './App';
-import configureStore from './redux/configureStore.js';
+import configureStore from './redux/configureStore';
 
 const store = configureStore;
 
